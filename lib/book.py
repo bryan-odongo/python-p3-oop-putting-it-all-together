a6f970adf12179027e6204eb2f53cdc6738cf1bd
odongo <saonarawestinc@outlook.com>
2024-06-02 09:00:29 +0300

    completed python-p3-oop-putting-it-all-together

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python3
 
+
 class Book:
-    pass
-    
-        
+    def __init__(self, title, pages=0):
+        self.title = title
+        self._pages = pages
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    @property
+    def page_count(self):
+        return self._pages
+
+    @page_count.setter
+    def page_count(self, value):
+        if isinstance(value, int):
+            self._pages = value
+        else:
+            print("page_count must be an integer")
